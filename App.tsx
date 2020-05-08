d831fbf78846c2c5179bb55b4518433a8e049990
Gonçalo Santos <gregueiras@gmail.com>
2020-05-08 22:49:11 +0100

    Add Sentry

--- App.tsx
+++ App.tsx
@@ -7,6 +7,15 @@ import { Provider } from "react-redux";
 import setupStore from "./src/redux/store";
 import { NavigationContainer } from "@react-navigation/native";
 
+import * as Sentry from "sentry-expo";
+import Constants from "expo-constants";
+
+Sentry.init({
+  dsn:
+    "https://c3d912011a1e4a98b114fdcc6a2f6465@o387238.ingest.sentry.io/5228960",
+});
+Sentry.setRelease(Constants?.manifest?.revisionId ?? "1.0");
+
 export default function MyApp() {
   const [loading, setLoading] = useState(true);
 
